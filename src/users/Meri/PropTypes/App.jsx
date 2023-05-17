919431890bc47206fc25f6c03a7ec0f0301f938e
meryarshakyan <arshakyanmery02@gmail.com>
2023-05-17 22:04:02 +0300

    homework 6

---
+++ src/users/Meri/PropTypes/App.jsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from 'react'
+import Component from './Component'
+
+export default function App() {
+    const [data, setData] = useState([]);
+    const [backData, setBackData] = useState([]);
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/todos')
+            .then(res => res.json())
+            .then(json => {
+                setData(json)
+                setBackData(json)
+            });
+    },[])
+    const change = (id) => {
+        const newdata = data.map((elem) => {
+            if(elem.id == id) {
+                elem.completed = !elem.completed;
+            }
+            return elem;
+        })
+        setData(newdata)
+    }
+    const redToGreen = () => {
+        const newdata = [...data];
+        newdata.sort((a,b) => (a.completed === b.completed ? '' : a.completed ? b.completed - a.completed : a.completed - b.completed))
+        setData(newdata);
+    }
+    const greenToRed = () => {
+        const newdata = [...data];
+        newdata.sort((a,b) => (a.completed === b.completed ? '' : b.completed ? a.completed - b.completed : b.completed - a.completed))
+        setData(newdata);
+    }
+    const back = () => {
+        setData(backData)
+    }
+    return (
+        <div>
+            <button onClick={redToGreen}>Red to Green</button>
+            <button onClick={greenToRed}>Green to red</button>
+            <button onClick={back}>Back to the original data</button>
+            <Component data={data} change={change}/>
+        </div>
+    )
+}
---
+++ src/users/Meri/PropTypes/Component.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import PropTypes from 'prop-types'
+import './Component.scss'
+
+export default function Component(props) {
+    return (
+        <div className='flex'>
+            {
+                props.data.map(elem => {
+                    return (
+                        <div key={elem.id} className='flex__item' onClick={() => props.change(elem.id)}>
+                            <h2>{elem.title}</h2>
+                            <span className={`flex__status ${elem.completed ? 'flex__status-success' : 'flex__status-failure'}`}></span>
+                        </div>
+                    )
+                })
+            }
+        </div>
+    )
+}
+Component.propTypes = {
+    data: PropTypes.arrayOf(
+        PropTypes.exact(
+            {
+                userId: PropTypes.number,
+                id: PropTypes.number,
+                title: PropTypes.string,
+                completed: PropTypes.bool
+            }
+        )
+    ),
+    change: PropTypes.func.isRequired
+}

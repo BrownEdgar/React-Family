fe339508f0dc872b31bbbbbb5d91e5ec000e8063
meryarshakyan <arshakyanmery02@gmail.com>
2023-05-22 19:11:08 +0300

    Mery's homeworkes

--- src/users/Meri/WorkWithImages/App.jsx
+++ src/users/Meri/WorkWithImages/App.jsx
@@ -1,29 +1,29 @@
 import {useEffect, useState } from 'react'
 import "./App.scss"
 export default function App() {
-    const [users, setUsers] = useState([]);
-    console.log(users)
+    const [products, setProducts] = useState([]);
     useEffect(() => {
-        fetch('https://dummyjson.com/users')
+        fetch('https://dummyjson.com/products')
         .then(res => res.json())
-        .then(data => setUsers(data))
+        .then(data => setProducts(data.products))
     },[])
     return (
         <div>
-            <div className='User'>
-                {
-                    users.map(user => {
-                        return (
-                            <div key={user.id} className='User__info'>
-                                <h2><span>title</span> {user.title}</h2>
-					            <h2><span>description</span> {user.description}</h2>
-					            <h2><span>price</span> {user.price}</h2>
-					            <h2><span>stock</span> {user.stock}</h2>
+            {
+                products.map(product => {
+                    return (
+                        <div className='User' key={product.id}>
+                            <img src={product.images[0]} alt="" />
+                            <div className='User__info'>
+                                <h2><span>title</span> {product.title}</h2>
+                                <h2><span>description</span> {product.description.slice(1, 10)}</h2>
+                                <h2><span>price</span> {product.price}</h2>
+                                <h2><span>stock</span> {product.stock}</h2>
                             </div>
-                        )
-                    })
-                }
-            </div>
+                        </div>
+                    )
+                })
+            }
         </div>
     )
 }

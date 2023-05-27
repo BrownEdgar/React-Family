d710b4f895c52f321c3101df1a3886a4264cf47f
Arman <arman.araghi07@mail.ru>
2023-05-27 11:16:36 +0400

    Arman's homework

---
+++ src/users/Arman/useContext Hw/App.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import useSimpleHook from "./useSimpleHook";
+export default function App() {
+    const todos = [
+        {
+            "userId": 1,
+            "id": 1,
+            "title": "delectus aut autem",
+            "completed": false
+          },
+          {
+            "userId": 1,
+            "id": 2,
+            "title": "quis ut nam facilis et officia qui",
+            "completed": false
+          },
+          {},
+          {},
+          {},
+          {  }
+    ]
+    const {
+        data,
+        addId,
+        allDone,
+        removeById,
+        addTodo,
+        toJson,
+    } = useSimpleHook(todos)
+    return (
+        <div>
+            <pre>{JSON.stringify(data, null, 1)}</pre>
+            <button onClick={allDone}>All completed</button>
+            <button onClick={addId}>Add id</button>    
+            {data.map((todo) => (
+            <button key={todo.id} disabled={!todo.id} onClick={() => removeById(todo.id)}>Delete {todo.id}</button>
+            ))}
+            <button onClick={addTodo}>Add todos</button>
+            <button onClick={toJson}>to string</button>
+        </div>
+    )
+} 
+    

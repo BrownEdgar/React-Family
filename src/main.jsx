38324f4d4b3ae48b5a8fd5d328659dcef9f7df7f
Alaward <133217093+Zorgood@users.noreply.github.com>
2023-06-05 00:15:51 +0400

    homework

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 
 import ReactDOM from 'react-dom/client'
-import App from './Edgar/Forms/Formik/formBookingexample/App'
+import App from './users/Zor/lesson-React/FormBookexample/App'
 import './index.css'
 
 

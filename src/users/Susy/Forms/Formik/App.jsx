49ba6439a9a21fb2e2b6831b8a22b75114f26047
SusyHayrapetyan <hayrapetyansusanna8@gmail.com>
2023-06-05 19:31:46 +0400

    booking

--- src/users/Susy/Forms/Formik/App.jsx
+++ src/users/Susy/Forms/Formik/App.jsx
@@ -1,47 +1,101 @@
-import { useState } from 'react'
+import {useState} from "react";
+import { Formik , Form , Field, ErrorMessage } from "formik";
+import {v4 as uuidv4} from 'uuid';
+import './App.scss'
 
-
-import "./App.scss"
+const initialValues = {
+    name: '',
+    phone: '',
+    arrivalDate: '',
+    departureDate: '',
+    adults: '',
+    children: ''
+}
 
 export default function App() {
-    const [users,setUsers] = useState([])
 
-    const handleClick = () => {
-        const email = document.getElementById('email').value
-        const password = document.getElementById('password').value
-        
-        const newUser = {
-            email: email,
-            password: password
+    const [bookinglist,setBookinglist] = useState([])
+    console.log(bookinglist)
+
+    const handleSubmit = (values, {formReset}) => {
+
+        const children = bookinglist.some(booking => booking.children === "4");
+
+        if (values.children === "4" && children) {
+            alert("Cannot book for a family with 4 children again");
+            return;
         }
+
+        const list = {
+            id: uuidv4(),
+            ...values
+        }
+        setBookinglist([...bookinglist,list])
+        formReset()
         
-        setUsers([...users, newUser])
-        
-        document.getElementById('email').value = ''
-        document.getElementById('password').value = ''
     }
     return (
-        <div>
-            <div className="container">
-                <h1>Вход</h1>
-                <form>
-                    <input type="email" name="" id="email" placeholder="Имя пользователя или Email" />
-                    <input type="password" name="" id="password" placeholder="Пароль" />
-                </form>
-                <h3>Забыли пароль?</h3>
-                <div className="container__buttons">
-                    <button className='blue'><span>Войти</span></button>
-                    <button onClick={handleClick} className='grey'><span>Регистрация</span></button>
-                </div>
+        <div className="container">
+            <div className="container-wraper">
+            <div className="container-form">
+                <h2>BOOK A ROOM</h2>
+            <Formik
+                initialValues={initialValues}
+                onSubmit={handleSubmit}
+                validateOnBlur={true}
+                validateOnChange={false}
+            >
+                {
+                    (formik) => {
+                        return (
+                            <Form>
+                                <div>
+                                    <Field type='text' id='name' name='name' placeholder='name'/>
+                                    <ErrorMessage name='name' component='p' className='error' />
+                                </div>
+                                <div>
+                                    <Field type='tel' id='phone' name='phone' placeholder='phone'/>
+                                    <ErrorMessage name='phone' component='p' className='error' />
+                                </div>
+                                <div>
+                                    <Field type='date' id='arrivalDate' name='arrivalDate' placeholder='arrivalDate'/>
+                                    <ErrorMessage name='arrivalDate' component='p' className='error' />
+                                </div>
+                                <div>
+                                    <Field type='date' id='departureDate' name='departureDate' placeholder='departureDate'/>
+                                    <ErrorMessage name='departureDate' component='p' className='error' />
+                                </div>
+                                <div className='w-50'>
+                                <div>
+                                    <Field as='select' id='adults' name='adults'>
+                                        <option value="" selected disabled>adults</option>
+                                        <option value="1">1</option>
+                                        <option value="2">2</option>
+                                        <option value="3">3</option>
+                                        <option value="4">4</option>
+                                    </Field>
+                                    <ErrorMessage name='adults' component='p' className='error' />
+                                </div>
+                                <div>
+                                    <Field as='select' id='children' name='children'>
+                                        <option value="" selected disabled>children</option>
+                                        <option value="1">1</option>
+                                        <option value="2">2</option>
+                                        <option value="3">3</option>
+                                        <option value="4">4</option>
+                                    </Field>
+                                    <ErrorMessage name='children' component='p' className='error' />
+                                </div>
+                                </div>
+                                <div className='last'>
+                                    <input type='submit' value='CHECK AVAILABILITY'/>
+                                </div>
+                            </Form>
+                        )
+                    }
+                }
+            </Formik>
             </div>
-            <div className='users'>
-                <h4>Users:</h4>
-                {users.map((user, index) => (
-                    <div key={index}>
-                        <p>Email: {user.email}</p>
-                        <p>Password: {user.password}</p>
-                    </div>
-                ))}
             </div>
         </div>
     )

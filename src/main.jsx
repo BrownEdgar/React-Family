786e0a8448dd1976476aba4cd1d17b70daf3abd7
NarekHarutyunyann <narek.harutyunyann@mail.ru>
2023-06-08 02:40:07 +0400

    homework 14

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-<<<<<<< HEAD
-
-import App from './users/Narek/Narek/less-9(axios)/App'
-
-
+import App from './users/Narek/Narek/les 14(router)/App'
 import './index.css'
-=======
-import App from './Edgar/App'
->>>>>>> 7670ca00a491085a4f2e370e65ec228eb794c2de
+import { BrowserRouter as Router} from 'react-router-dom'
+
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  
-    <App />
+  <Router>
+      <App />
+  </Router>
   
 )

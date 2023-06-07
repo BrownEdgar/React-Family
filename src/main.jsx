a3da1f3cd22d2af373cfd5d427e40f3bf8e1cf62
NarekHarutyunyann <narek.harutyunyann@mail.ru>
2023-06-08 02:41:29 +0400

    les 14

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './users/Narek/Narek/les 14(router)/App'
+import App from './Edgar/01/App'
 import './index.css'
 import { BrowserRouter as Router} from 'react-router-dom'
 

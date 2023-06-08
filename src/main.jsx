6fb0658fceedd165274895f1f3674f7bc6fd925c
NarekHarutyunyann <narek.harutyunyann@mail.ru>
2023-06-08 15:12:56 +0400

    router

--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,12 @@
 
 import ReactDOM from 'react-dom/client'
-<<<<<<< HEAD
-import App from './Edgar/01/App'
-import './index.css'
 import { BrowserRouter as Router} from 'react-router-dom'
-
-
-=======
-import App from './Edgar/Forms/Formik/formBookingexample/App'
+import App from './users/Meri/React-router6/App'
 import './index.css'
-import { BrowserRouter as Router } from 'react-router-dom'
->>>>>>> 737d22dda13a54b5d77913222b8bb5258b19b9cc
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Router>
-<<<<<<< HEAD
       <App />
   </Router>
-  
-=======
-    <App />
-</Router>
->>>>>>> 737d22dda13a54b5d77913222b8bb5258b19b9cc
 )
--- src/users/Meri/React-router6/pages/Album.jsx
+++ src/users/Meri/React-router6/pages/Album.jsx
@@ -6,19 +6,17 @@ export default function Album() {
   const [album, setAlbum] = useState([])
   const {id} = useParams()
   useEffect(() => {
-      axios(`https://jsonplaceholder.typicode.com/photos?albumId=${id}`)
+      axios(`https://jsonplaceholder.typicode.com/photos/${id}`)
         .then(res => setAlbum(res.data))
   },[id])
 	return (
 		<div>
 			<h1>Welcome to post N {id}</h1>
-            {album.map(item => (
-                <div key={item.id}>
-                    <h1>{item.title}</h1>
+                <div key={album.id}>
+                    <h1>{album.title}</h1>
                     <h2>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam, laudantium a. Odit architecto praesentium quisquam?</h2>
-                    <img src={item.url} alt={item.title} />
+                    <img src={album.url} alt={album.title} />
                 </div>
-            ))}
 		</div>
 	)
 }

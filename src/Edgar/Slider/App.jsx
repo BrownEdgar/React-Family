04153b0efbd45e82f6e3c987399c1f46237cc601
BrownEdgar <nikogosjanedgar@gmail.com>
2023-06-18 11:39:37 +0400

    post slider done

--- src/Edgar/Slider/App.jsx
+++ src/Edgar/Slider/App.jsx
@@ -6,24 +6,25 @@ import Post from './Post';
 import './App.scss'
 
 export default function App() {
-	const [state, dispatch] = useReducer(reducer, initialState)
+  const [state, dispatch] = useReducer(reducer, initialState)
 
-	const settings = {
-		dots: false,
-		infinite: true,
-		speed: 500,
-		slidesToShow: 3,
-		slidesToScroll: 1
-	};
-	return (
-		<div className='App'>
-			<div className="App-container">
-				<Slider {...settings}>
-					{
-						state.map(post => <Post key={post.id} post={post}/>)
-					}
-				</Slider>
-			</div>
-		</div>
-	)
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+  }
+
+  return (
+    <div className='App'>
+      <div className="App-container">
+        <Slider {...settings}>
+          {
+            state.map(post => <Post key={post.id} post={post} />)
+          }
+        </Slider>
+      </div>
+    </div>
+  )
 }

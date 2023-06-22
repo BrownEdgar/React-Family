068e980ab5a37974114fcdc8becfb53f74895d86
BrownEdgar <nikogosjanedgar@gmail.com>
2023-06-22 16:34:17 +0400

    clear branch after reset

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,7 @@
-import { useSelector,useDispatch } from "react-redux"
-import Posts from "./components/Posts";
-
-
 export default function App() {
-
-	const store = useSelector((store) => store);
-	const dispatch = useDispatch()
-	const handleClick = (type) => {  
-		dispatch({type})
-	
-	}
 	return (
 		<div>
-			<h1> welcome to Redux </h1>
-			<pre>
-				{JSON.stringify(store,null,1)}
-			</pre>
-			<button onClick={() => handleClick('plus')}>add count</button>
-			<button onClick={() => handleClick('minus')}>minus count</button>
-			<Posts />
+
 		</div>
 	)
 }

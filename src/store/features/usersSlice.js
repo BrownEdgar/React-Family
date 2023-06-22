8da46a015a4a542b66f8bfa7ed0250e3eadbbf64
BrownEdgar <nikogosjanedgar@gmail.com>
2023-06-22 16:24:27 +0400

    clear redux branch

--- src/store/features/usersSlice.js
+++
@@ -1,8 +0,0 @@
- const initialUsersValue = [];
-
-export default function usersReducer(state = initialUsersValue, action) {
-	switch (action.type) {
-		case 'plus': return ["Anahit"];
-		default: return state
-	}
-}
--- src/store/store.js
+++ src/store/store.js
@@ -1,14 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-import countReducer from "./features/countSlice";
-import usersReducer from "./features/usersSlice";
+
 import postReducer from "./features/postsSlice";
 
 
  export const store = configureStore({
 	reducer:{
-		count: countReducer,
-		users: usersReducer,
 		posts: postReducer
-
 	}
 })

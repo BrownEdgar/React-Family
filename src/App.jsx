ef4bfc4288f1850cfdc1457e2ca7a69dd6601ff9
BrownEdgar <nikogosjanedgar@gmail.com>
2023-06-24 16:29:56 +0400

    added redux middleware

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,29 @@
+import Posts from "./components/Posts";
+
+import './App.css'
+import { useDispatch } from "react-redux";
+import { addPost } from "./store/features/postsSlice";
+
 export default function App() {
+	const dispatch = useDispatch()
+	const addPostAction = () => {
+		const post = {
+			id: Date.now(),
+			title: 'Lorem ipsum dolor sit.',
+			body: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora soluta quas debitis.',
+			userId: 5
+		}
+
+		dispatch({ type: 'add-post' })
+
+	}
 	return (
-		<div>
+		<div className="App">
+			<button
+				onClick={addPostAction}
+			>add random post</button>
+			<Posts />
+
 
 		</div>
 	)
